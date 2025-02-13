a57ff95b5f9bdb3fb54285c192d10a6d729f89e7
ohyecloudy <ohyecloudy@gmail.com>
2025-02-13 23:48:56 +0900

    feat(config): use config_env
    runtime.exs에도 사용할 수 있는 config_env를 config 파일에는 일관되게
    사용

--- config/config_template.exs
+++ config/config_template.exs
@@ -1,10 +1,10 @@
 import Config
 
-import_config "#{Mix.env()}.exs"
+import_config "#{config_env()}.exs"
 
 # import secret config
 umbrella_root = Path.dirname(Path.expand(__DIR__))
-secret_config_path = Path.join([umbrella_root, "config", "#{Mix.env()}.secret.exs"])
+secret_config_path = Path.join([umbrella_root, "config", "#{config_env()}.secret.exs"])
 
 if File.exists?(secret_config_path) do
   import_config secret_config_path
